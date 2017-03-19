fe713638b6215eb8bb6e8ba0f43394b78a655c7f
mml1988 <841423845@qq.com>
2017-03-19 20:58:54 +0800

    clean up & using routines & using regexp

--- dir/dir.go
+++ dir/dir.go
@@ -3,23 +3,29 @@ package dir
 import (
 	"os"
 	"path/filepath"
+	"regexp"
 )
 
 type FileInfo struct {
 	FileName string
 	FileSize int64
+	FileSha1 string
 }
 
 func WalkDir(dirPath, filter string) (files []FileInfo, err error) {
-	files = make([]FileInfo, 0, 30)
+	files = make([]FileInfo, 0, 3000)
 	//filter = strings.ToUpper(filter)
 	walkFunc := func(filename string, fi os.FileInfo, err error) error {
-		if fi.IsDir() {
-			return nil
-		}
 		/* 根据filter过滤文件和目录 */
-		fileInfo := FileInfo{filename, fi.Size()}
-		files = append(files, fileInfo)
+		match, _ := regexp.MatchString(filter, filename)
+		if filter == "" || true != match {
+			if fi.IsDir() {
+				return nil
+			}
+			fileInfo := FileInfo{filename, fi.Size(), ""}
+			files = append(files, fileInfo)
+		}
+
 		return nil
 	}
 	err = filepath.Walk(dirPath, walkFunc)
--- listFileHash/listFileHash.go
+++ listFileHash/listFileHash.go
@@ -7,21 +7,34 @@ import (
 	"sha1"
 )
 
-//http://www.yiibai.com/go/golang-reading-files.html
+var root *string = flag.String("r", "G:\\MML", "Root directory for search")
+var filter *string = flag.String("f", "*", "Specify which files to skip ")
+var workers *int = flag.Int("w", 16, "Specify how many workers to work")
 
-var root *string = flag.String("r", "/root", "Root directory for search")
-var filter *string = flag.String("f", "", "Specify which files to skip ")
-var workers *int = flag.Int("w", 8, "Specify how many workers to work")
-
+func worker(FirstIndex int, LastIndex int, files []dir.FileInfo, result chan bool) {
+	for i := FirstIndex; i < LastIndex; i++ {
+		files[i].FileSha1, _ = sha1.SHA1File(files[i].FileName)
+	}
+	result <- true
+	fmt.Printf("files from %d to %d have down.\r\n", FirstIndex, LastIndex)
+}
 func main() {
 	flag.Parse()
 
 	if root != nil {
-		fmt.Println("root =", *root, "filter =", *filter, "workers =", *workers)
+		//fmt.Println("root =", *root, "filter =", *filter)
 		files, _ := dir.WalkDir(*root, *filter)
-		for _, file := range files {
-			var sha1Str, _ = sha1.SHA1File(file.FileName)
-			fmt.Printf("%s,%s,%d\r\n", file.FileName, sha1Str, file.FileSize)
+
+		results := make([]chan bool, *workers)
+		for i := 0; i < *workers; i++ {
+			results[i] = make(chan bool)
+			go worker(i*(len(files)/(*workers)), (i+1)*(len(files)/(*workers)), files, results[i])
+		}
+		for _, result := range results {
+			<-result
+		}
+		for _, fileInfo := range files {
+			fmt.Printf("%s,%s,%d\r\n", fileInfo.FileName, fileInfo.FileSha1, fileInfo.FileSize)
 		}
 	}
 }
